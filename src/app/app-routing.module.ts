2ccaa841a616d4a277b3b42c9079c3cb5d3f056b
Pouya753 <shiripoya@gmail.com>
2024-04-23 00:45:17 -0700

    app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {AuthModule} from "./pages/auth/auth.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./pages/product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then((m) => m.ProfileModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'error/404',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
